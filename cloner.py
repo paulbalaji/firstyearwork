fb127d6353755bd78d0a94b36b25807a3ed948a3
Paul Balaji <pmb14@ic.ac.uk>
2015-12-09 12:19:36 +0000

    removed folders again
    Signed-off-by: Paul Balaji <pmb14@ic.ac.uk>

--- cloner.py
+++ cloner.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     w = open('todo.sh', 'w')
     with open('clone links') as f:
         for line in f:
-            command = "git clone " + line
+            command = "git submodule add " + line
             w.write(command)
     w.close()
--- todo.sh
+++ todo.sh
@@ -1,17 +1,17 @@
-git clone https://gitlab-old.doc.ic.ac.uk/g1416302/topics-website.git
-git clone https://gitlab-old.doc.ic.ac.uk/hopfield-network-jpgsa/hopfield_network_topics.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_spring/javadictionary_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/haskellcalculus_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/haskelllsystems_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/haskellmacroprocessor_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/haskellrecursion_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/haskellquadratic_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_spring/assemblermatrixmultiplication_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_spring/javaspreadsheet_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_spring/sqlfilmqueries_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_spring/javadiscreteeventsimulation_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_spring/javaturtleinterpreter_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/javarpsls_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_spring/javapictureprocessing_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/javaloopsandarrays_pmb14.git
-git clone https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/javarecursion_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/g1416302/topics-website.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/hopfield-network-jpgsa/hopfield_network_topics.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_spring/javadictionary_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/haskellcalculus_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/haskelllsystems_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/haskellmacroprocessor_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/haskellrecursion_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/haskellquadratic_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_spring/assemblermatrixmultiplication_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_spring/javaspreadsheet_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_spring/sqlfilmqueries_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_spring/javadiscreteeventsimulation_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_spring/javaturtleinterpreter_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/javarpsls_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_spring/javapictureprocessing_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/javaloopsandarrays_pmb14.git
+git submodule add https://gitlab-old.doc.ic.ac.uk/lab1415_autumn/javarecursion_pmb14.git
